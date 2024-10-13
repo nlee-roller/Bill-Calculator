5b6de6c4fcc90738362004e783bf73cc9025a619
Nick Lee-Roller <leerolle@usc.edu>
2024-10-13 13:52:50 -0700

    Initial Commit

---
+++ Lee-RollerNickHW4/Lee_RollerNickHW4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lee_RollerNickHW4App.swift
+//  Lee-RollerNickHW4
+//
+//  Created by Nick Lee-Roller on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Lee_RollerNickHW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
